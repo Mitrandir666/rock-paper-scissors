139f60b797e1b46a353436bfd37000eba9c8624d
mitrandir666 <a.t.szypryt@gmail.com>
2024-01-26 00:32:12 +0100

    final versions with repeatable game() function

--- program.js
+++ program.js
@@ -1,35 +1,77 @@
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
-let playerPoints = 0;
-let computerPoints =0;
 
 
-function getComputerChoice(){                                               // randomly taking weapon name from an array for computerplayer
-    let weaponArray = ['rock', 'paper', 'scissors'];
-    let computer = weaponArray[Math.floor(Math.random()*3)];
-    return computer;
-}
+function game(){
 
-function getPlayerChoice(){                                                 // player chooses his weapon
-    let weaponArray = ['rock', 'paper', 'scissors'];
-    let player = weaponArray[prompt('0 = rock, 1 = paper, 2 = scissors. Chose Your weapon wisely: ')];
-    return player;
-}
+    let playerPoints = 0;
+    let computerPoints =0;
 
-function playRound(playerSelection, computerSelection){                     //function compares players and computers weapons and calculate the verdict.
-    if (playerSelection===computerSelection){
-        playerPoints=playerPoints + 1;
-        computerPoints=computerPoints +1;
-        return 'DRAW! ' + playerSelection +' is equal ' + computerSelection;
-    }else if ((playerSelection==='rock' && computerSelection==='scissors')||(playerSelection==='paper' && computerSelection==='rock')||(playerSelection==='scissors' && computerSelection==='paper')){
-        playerPoints=playerPoints +1;
-        return 'player wins! '+ playerSelection+ ' beat '+ computerSelection;
-    }else{
-        computerPoints=computerPoints +1;
-        return 'computer wins '+ computerSelection+ ' beat '+ playerSelection;
+    function getComputerChoice(){                                               // randomly taking weapon name from an array by computerplayer
+        let weaponArray = ['rock', 'paper', 'scissors'];
+        return weaponArray[Math.floor(Math.random()*3)]; 
     }
+    
+    function getPlayerChoice(){                                                 // player chooses his weapon
+        let weaponArray = ['rock', 'paper', 'scissors'];
+        return weaponArray[prompt('0 = rock, 1 = paper, 2 = scissors. Chose Your weapon wisely: ')];
+    }   
+
+    function playRound(playerSelection, computerSelection){                     //function compares players and computers weapons and calculate the verdict.
+        if (playerSelection===computerSelection){
+            playerPoints=playerPoints + 1;
+            computerPoints=computerPoints +1;
+            return 'DRAW! ' + playerSelection +' is equal ' + computerSelection;
+        }else if ((playerSelection==='rock' && computerSelection==='scissors')||(playerSelection==='paper' && computerSelection==='rock')||(playerSelection==='scissors' && computerSelection==='paper')){
+            playerPoints=playerPoints +1;
+            return 'player wins! '+ playerSelection+ ' beat '+ computerSelection;
+        }else{
+            computerPoints=computerPoints +1;
+            return 'computer wins '+ computerSelection+ ' beat '+ playerSelection;
+        }
+    }
+    
+        for( let i=1;i<=5;i++){                                                 // five rounds loop
+            console.log('round ' + i+'/5');
+            let playerSelection = getPlayerChoice();
+            let computerSelection = getComputerChoice();
+
+            if (playerSelection!=undefined){
+                console.log(playRound(playerSelection, computerSelection));
+                console.log(playerPoints + ' ' + computerPoints);
+            }else{
+                console.log('You have no weapon! - no points');
+            }
+            console.log('masz ' + playerPoints + ' punktow, przeciwnik ma ' + computerPoints + ' punktow');
+        }
+    if(playerPoints>computerPoints){                                        // game() returns result
+        return console.log('Player won');
+    }else if (playerPoints<computerPoints){
+        return console.log('Computer won');
+    }else
+        return console.log('Draw');
+   
+
 }
-console.log(playRound(playerSelection, computerSelection));
+
+game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
